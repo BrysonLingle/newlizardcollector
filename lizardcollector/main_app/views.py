3187430029432d2b39d46fdcd96612e36de0795f
Bryson lingle <brysonburkett99@gmail.com>
2023-09-22 08:23:15 -0600

    updating

---
+++ lizardcollector/main_app/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+
+
+lizard_data = [
+    {
+        'name': 'Green Anole',
+        'color': 'Green',
+        'size': 'Small',
+        'description': 'The green anole is a small lizard found in the southeastern United States.'
+    },
+    {
+        'name': 'Bearded Dragon',
+        'color': 'Various',
+        'size': 'Medium',
+        'description': 'Bearded dragons are popular pet lizards known for their beard-like scales.'
+    },
+   
+]
+
+def about(request):
+    return render(request, 'about.html')
+
+def all_lizards(request):
+    return render(request, 'all_lizards_html')
+
+def all_lizards(request):
+    return render(request,'lizard.html',{'lizard': lizard_data})
+
+def home(request):
+   return render(request, 'main_app/home.html')
+
